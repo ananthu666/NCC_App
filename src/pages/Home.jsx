4efe2e677531f0db8614296a1234a28b86d592ca
Adithyan T <adithsreedhar628@gmail.com>
2024-02-17 22:42:23 +0530

    Updated Table

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,14 @@ import Table from "../components/Table";
 
 import { database } from "../../firebase";
 import { useState, useEffect } from "react";
-import { doc, collection, getDocs, addDoc, setDoc ,updateDoc } from "firebase/firestore";
-
+import {
+  doc,
+  collection,
+  getDocs,
+  addDoc,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
 
 const Home = () => {
   const [cadets, setcadets] = useState([]);
@@ -14,18 +20,18 @@ const Home = () => {
   async function getCadets(db = database) {
     const cadetsData = [];
     const querySnapshot = await getDocs(collection(db, "cadets"));
-    querySnapshot.forEach((doc) => {
-     // doc.data() is never undefined for query doc snapshots
-     console.log(doc.id, " => ", doc.data());
-    });
+    // querySnapshot.forEach((doc) => {
+    //   // doc.data() is never undefined for query doc snapshots
+    //   console.log(doc.id, " => ", doc.data());
+    // });
 
-   //  querySnapshot.forEach((doc) => {
-   //    const cadet = {
-   //    id: doc.id,
-   //    ...doc.data()
-   //  };
-   //  cadetsData.push(cadet);
-   // });
+    querySnapshot.forEach((doc) => {
+      const cadet = {
+        id: doc.id,
+        ...doc.data(),
+      };
+      cadetsData.push(cadet);
+    });
     return cadetsData;
   }
 
@@ -41,42 +47,37 @@ const Home = () => {
   }
 
   async function addCadet(db = database) {
-    await setDoc(doc(db, "cadets", "UP27SDN111968"),{
-      "college": "College of Engineering, Tvpm",
-      "dateOfBirth": "04-12-2001",
-      "dateOfDischarge": "31-03-2024",
-      "dateOfEnrolment": "22-12-2021",
-      "dateOfPassingCertificateBExam": "27-03-2023",
-      "detailsOfCampsAttended": [
-      "ATC"
-      ],
+    await setDoc(doc(db, "cadets", "UP27SDN111968"), {
+      college: "College of Engineering, Tvpm",
+      dateOfBirth: "04-12-2001",
+      dateOfDischarge: "31-03-2024",
+      dateOfEnrolment: "22-12-2021",
+      dateOfPassingCertificateBExam: "27-03-2023",
+      detailsOfCampsAttended: ["ATC"],
       "father'sName": "S SREEHARSHAN",
-      "name": "ALOK S NAIR",
-      "rank": "NC1"
-      });
+      name: "ALOK S NAIR",
+      rank: "NC1",
+    });
   }
-
 
   async function updateCadet(db = database) {
     try {
-        const cadetDocRef = doc(db, "cadets", "KL27SDN111968");
-        await updateDoc(cadetDocRef, {
-            "dateOfDischarge": "31-03-2024",
-            "rank": "Martial" 
-        });
-        console.log("Cadet data updated successfully");
+      const cadetDocRef = doc(db, "cadets", "KL27SDN111968");
+      await updateDoc(cadetDocRef, {
+        dateOfDischarge: "31-03-2024",
+        rank: "Martial",
+      });
+      console.log("Cadet data updated successfully");
     } catch (error) {
-        console.error("Error updating cadet data:", error);
+      console.error("Error updating cadet data:", error);
     }
-}
+  }
 
   useEffect(() => {
-    
     //addCadet();
     //updateCadet();
     fetchCadets();
   }, []);
-
 
   return (
     <>
